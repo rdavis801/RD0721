6f9a2a5e803d61ac3a8c3a58b6c6d5bdf463d3fd
rdavis801 <rcd500@yahoo.com>
2021-07-23 09:45:58 -0400

    adds datesHelper class to determine a day's 'weekness' and determine if a holiday falls within a given range

---
+++ src/main/java/tech/ronalddavis/dates/DatesHelper.java
@@ -0,0 +1,100 @@
+package tech.ronalddavis.dates;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class DatesHelper {
+
+    public static int findWeekends(LocalDate startDate, LocalDate endDate) {
+        return (int) startDate.datesUntil(endDate)
+                .filter(t -> (t.getDayOfWeek() == DayOfWeek.SATURDAY || t.getDayOfWeek() == DayOfWeek.SUNDAY))
+                .count();
+    }
+
+    public static int findWeekdays(LocalDate startDate, LocalDate endDate) {
+        return (int) startDate.datesUntil(endDate)
+                .filter(t -> (t.getDayOfWeek() != DayOfWeek.SATURDAY && t.getDayOfWeek() != DayOfWeek.SUNDAY))
+                .count();
+    }
+
+    /*
+     * Returns if Independence Day falls within the given range
+     * Assumes that only one Independence Day is present
+     */
+    public static boolean fallsOnIndependenceDay(LocalDate startDate, LocalDate endDate) {
+
+        return startDate.datesUntil(endDate)
+                .filter(t -> Objects.requireNonNull(getIndependenceDays(startDate.getYear(), endDate.getYear())).contains(t))
+                .count() >= 1;
+
+    }
+
+    /*
+     * Returns if Labor Day falls within the given range
+     * Assumes that only one Labor Day is present
+     */
+    public static boolean fallsOnLaborDay(LocalDate startDate, LocalDate endDate) {
+
+        return startDate.datesUntil(endDate)
+                .filter(t -> Objects.requireNonNull(getLaborDays(startDate.getYear(), endDate.getYear())).contains(t))
+                .count() >= 1;
+    }
+
+    /*
+     * Returns a list of zero or more LocalDate items referring to the observed Independence Days within the given years
+     * Observed Independence Day is defined as July 4th, if it falls on a weekday, or if July 4th falls on a weekend,
+     * it is observed on the closest weekday
+     */
+    private static List<LocalDate> getIndependenceDays(int startDate, int endDate) {
+        if (startDate < 0 || endDate < 0 || startDate > endDate) {
+            return null;
+        }
+
+        final int observedDay = 4;
+        final int holidaysNeeded = 1 + (startDate - endDate);
+        List<LocalDate> dates = new ArrayList<>(holidaysNeeded);
+
+        for (int i = 0; i < holidaysNeeded; i++) {
+            LocalDate independenceDay = LocalDate.of(i + startDate, Month.JULY, observedDay);
+
+            switch (DayOfWeek.of(independenceDay.get(ChronoField.DAY_OF_WEEK))) {
+                case SATURDAY:
+                    dates.add(LocalDate.of(startDate, Month.JULY, observedDay - 1));
+                case SUNDAY:
+                    dates.add(LocalDate.of(startDate, Month.JULY, observedDay + 1));
+                default:
+                    dates.add(LocalDate.of(startDate, Month.JULY, observedDay));
+            }
+        }
+
+        return dates;
+    }
+
+    /*
+     * Returns a list of zero or more LocalDate items referring to the observed Labor Days within the given years
+     * Observed Labor Day is defined as the first Monday in September
+     */
+    private static List<LocalDate> getLaborDays(int startDate, int endDate) {
+        if (startDate < 0 || endDate < 0 || startDate > endDate) {
+            return null;
+        }
+
+        final int holidaysNeeded = 1 + (startDate - endDate);
+        List<LocalDate> dates = new ArrayList<>(holidaysNeeded);
+
+        for (int i = 0; i < holidaysNeeded; i++) {
+            // Start at the first of the month and store the first Monday
+            LocalDate beforeLaborDay = LocalDate.of(i + startDate, Month.AUGUST, 31);
+            dates.add(beforeLaborDay.datesUntil(beforeLaborDay.plusDays(7))
+                     .filter(t -> t.getDayOfWeek() == DayOfWeek.MONDAY)
+                     .findFirst().orElse(null));
+        }
+
+        return dates;
+    }
+}
